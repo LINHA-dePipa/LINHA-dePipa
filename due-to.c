dd339043a42905ed46ffd2f7bc8693c2e7e60f67
LINHA-dePipa <nova.conta.denovooo@gmail.com>
2025-02-15 19:43:59 -0300

    Add files via upload
    spiremental

---
+++ due-to.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+// GOES RANDOMIC BTW
+#define LIL char
+#define image '@'
+
+LIL literal_sublime(char* lil, char sub) {
+    char non_literal = 0;
+    char token = 0;
+    //sscanf(lil,"%d",&num);
+    char tok[] = ";";//59
+    while(lil[sub]<57) {
+        non_literal = non_literal*10+(lil[sub]-48);
+
+        //num*=10;
+        sub++;
+    }
+
+    return non_literal;
+}
+
+LIL rebuild(char *lil, char sub, char due, char mental) {
+    if(mental==image) {
+        sub++;
+        char twin = literal_sublime(lil, sub);
+        while(due<twin) {
+            due++;
+            //fine
+        }
+    }
+    return due;
+}
+
+LIL due_to(char *lil, char sub, char mental) {
+    sub++;
+    mental = lil[sub];
+    char twin = rebuild(lil, sub, literal_sublime(lil, sub), mental);
+    return twin;
+}
+
+void ddd(char*dfault) {
+    printf("%s", dfault);
+}
+
+LIL draw_sublime(char *lil, short height) {
+    char i = 0;
+
+    char exact = due_to(lil,i,exact);
+    ddd(" *****************");
+    /* Draw each-port */
+    for (i; i <= height; i++) {
+        if(i==2) {
+            ddd("*   LITERAL        # *\n");
+        }
+        if(i>5) {
+            ddd("* @                ");
+        } else {
+            ddd("*                 ");
+        }
+        if(i==exact) {
+            printf("%i", exact);
+        } else {
+            ddd("@");
+        }
+
+        ddd("  *\n");
+    }
+    ddd(" *********************\n >>_");
+    return exact;
+}
+
+int draw(char*lil, short infinite) {
+    while(0<infinite-10500)
+        infinite++;
+    system("clear");
+    ddd("SUBLIME BOARD\n");
+    LIL fictional_devil = draw_sublime(lil,10);
+    //scanf("%s",lil);
+    ddd("\n >> sublime.literal(");
+    printf("%d",fictional_devil);
+    ddd("^1) -- ");
+    return draw(lil, infinite+1);
+}
+
+int main() {
+    char lil[] = "0@255;";
+    return draw(lil,0);
+}
+
+/*
+13^1;
+1"o, 13^1, 1"o, 13^0;
+(13^1,9:00,13^0)"o;
+(1^1,,++1^1)"$;
+board.powerOn(13);
+board.blink();
+*/
